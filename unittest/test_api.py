128eebd1e866136fa99dce7230a7df2ad9bebafb
tuongtn <tuongtn@kalapa.vn>
2024-11-24 10:27:08 +0700

    update: unittest

--- unittest/test_api.py
+++ unittest/test_api.py
@@ -1,17 +1,22 @@
 import sys
 import json
 import requests
+import unittest
 
 sys.path.insert(0, ".")
 
 from configs.elk_config import app_config
 
-url = f'http://{app_config.BASE_URL}:{app_config.API_PORT}/send_data'
-print(f"server url: {url}")
-headers = {'Content-type': 'application/json', 'Authorization': '123456789'}
-data = json.load(open('data/messages/logs.json'))
-print(f"request data: {data}")
 
-res = requests.post(url, data=json.dumps(data, ensure_ascii=False).encode('utf-8'), headers=headers).json()
+class Test_API(unittest.TestCase):
+    def test_flask_api(self):
+        url = f'http://{app_config.BASE_URL}:{app_config.API_PORT}/send_data'
+        print(f"server url: {url}")
+        headers = {'Content-type': 'application/json', 'Authorization': '123456789'}
+        data = json.load(open('data/messages/logs.json'))
+        res = requests.post(url, data=json.dumps(data, ensure_ascii=False).encode('utf-8'), headers=headers).json()
+        print(res)
+        
 
-print(res)
+if __name__ == "__main__":
+    unittest.main()
